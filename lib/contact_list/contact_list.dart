7899b00cea2edce6d5ab339d8230ed1c24dbb460
Gurleen Sethi <sarusethi@rocketmail.com>
2018-12-07 21:41:40 +0530

    Show menu panel when item clicked

--- lib/contact_list/contact_list.dart
+++ lib/contact_list/contact_list.dart
@@ -1,9 +1,13 @@
 import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttery_contacts/common/colors.dart';
 import 'package:fluttery_contacts/common/spaces.dart';
 import 'package:fluttery_contacts/contact_detail/contact_detail_page.dart';
 
-class ContactListPage extends StatelessWidget {
+typedef OnMenuOpened(ContactListItemState widget);
+typedef OnMenuClosed();
+
+class ContactListPage extends StatefulWidget {
   final List<Contact> contacts;
   final ScrollController controller;
 
@@ -15,14 +19,28 @@ class ContactListPage extends StatelessWidget {
         super(key: key);
 
   @override
+  ContactListPageState createState() {
+    return new ContactListPageState();
+  }
+}
+
+class ContactListPageState extends State<ContactListPage> {
+  // Item that is currently selected (menu layer is shown).
+  int _selectedItem = -1;
+
+  // State object of the item which is selected.
+  ContactListItemState selectedItemState;
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      controller: controller,
+      controller: widget.controller,
       shrinkWrap: true,
       itemBuilder: (builder, index) {
-        final contact = contacts[index];
+        final contact = widget.contacts[index];
         EdgeInsets topMargin;
 
+        // Add padding to the top most element
         if (index == 0) {
           topMargin = EdgeInsets.only(top: 16.0);
         } else {
@@ -31,34 +49,116 @@ class ContactListPage extends StatelessWidget {
 
         return Container(
           margin: topMargin,
-          child: _ContactListItem(contact: contact),
+          child: ContactListItem(
+            contact: contact,
+            isSelected: index == _selectedItem,
+            onMenuClosed: () {
+              _selectedItem = -1;
+            },
+            onMenuOpened: (state) {
+              if (_selectedItem != index) {
+                print("$_selectedItem : ${selectedItemState?.mounted}");
+                if (selectedItemState?.mounted == true) {
+                  selectedItemState?.closeMenu();
+                }
+              }
+
+              selectedItemState = state;
+              _selectedItem = index;
+            },
+          ),
         );
       },
-      itemCount: contacts.length,
+      itemCount: widget.contacts.length,
     );
   }
 }
 
-class _ContactListItem extends StatefulWidget {
+class ContactListItem extends StatefulWidget {
   final Contact contact;
+  final OnMenuOpened onMenuOpened;
+  final OnMenuClosed onMenuClosed;
+  final bool isSelected;
 
-  const _ContactListItem({Key key, this.contact})
-      : assert(contact != null),
+  const ContactListItem({
+    Key key,
+    this.contact,
+    this.onMenuOpened,
+    this.onMenuClosed,
+    this.isSelected,
+  })  : assert(contact != null),
         super(key: key);
 
   @override
-  _ContactListItemState createState() {
-    return new _ContactListItemState();
+  ContactListItemState createState() {
+    return new ContactListItemState();
   }
 }
 
-class _ContactListItemState extends State<_ContactListItem>
+class ContactListItemState extends State<ContactListItem>
     with TickerProviderStateMixin {
+  // Is the menu layer shown
+  bool _isMenuVisible = false;
+  AnimationController _menuAnimationController;
+  Animation<double> _opacityAnimation;
+
+  // Key to be used for base item to calculate the width/height when menu is shown
+  GlobalKey _containerKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _menuAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 250));
+
+    _menuAnimationController.addListener(() => setState(() {}));
+
+    _opacityAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_menuAnimationController);
+
+    if (widget.isSelected) {
+      _isMenuVisible = true;
+
+      _menuAnimationController.value = 1.0;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  /// Show/Hide the menu layer
+  void toggleMenuOptions() {
+    if (_isMenuVisible) {
+      widget.onMenuClosed();
+      _menuAnimationController.reverse();
+    } else {
+      widget.onMenuOpened(this);
+      _menuAnimationController.forward();
+    }
+
+    _isMenuVisible = !_isMenuVisible;
+  }
+
+  void closeMenu() {
+    _menuAnimationController.reverse();
+    _isMenuVisible = false;
+  }
+
   @override
   Widget build(BuildContext context) {
+    // When list is scrolled, state of off-screen items is destroyed.
+    // If the item is selected and is returning to the visible screen then
+    // fire the callback so that the parent element has access to the newly
+    // created state and can manipulate it as required.
+    if (_isMenuVisible) {
+      widget.onMenuOpened(this);
+    }
+
     return Container(
-      margin: EdgeInsets.symmetric(
-          vertical: 4.0, horizontal: 16.0),
+      margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
       child: Hero(
         tag: widget.contact.displayName,
         child: Material(
@@ -68,61 +168,147 @@ class _ContactListItemState extends State<_ContactListItem>
           elevation: 1.0,
           child: InkWell(
             borderRadius: BorderRadius.circular(6.0),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return ContactDetailPage(
-                      contact: widget.contact,
-                    );
-                  },
+            onTap: toggleMenuOptions,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  key: _containerKey,
+                  padding: EdgeInsets.all(12.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[100],
+                          shape: BoxShape.circle,
+                        ),
+                        padding: EdgeInsets.all(4.0),
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.grey[400],
+                          size: 36.0,
+                        ),
+                      ),
+                      Spaces.w12,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Contact",
+                              //widget.contact.displayName,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Spaces.h4,
+                            Text(
+                              //widget.contact.phones.toList()[0].value,
+                              "1234567890",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              );
-            },
-            child: Container(
-              padding: EdgeInsets.all(12.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[100],
-                      shape: BoxShape.circle,
-                    ),
-                    padding: EdgeInsets.all(4.0),
-                    child: Icon(
-                      Icons.person,
-                      color: Colors.grey[400],
-                      size: 36.0,
+                Positioned(
+                  right: 0,
+                  child: Opacity(
+                    opacity: _opacityAnimation.value,
+                    child: Container(
+                      width: _opacityAnimation.value *
+                          (_containerKey?.currentContext
+                                  ?.findRenderObject()
+                                  ?.semanticBounds
+                                  ?.width ??
+                              0),
+                      height: (_containerKey?.currentContext
+                              ?.findRenderObject()
+                              ?.semanticBounds
+                              ?.height ??
+                          0),
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6.0),
+                        gradient: LinearGradient(
+                          colors: [
+                            AppColors.gradient2,
+                            AppColors.gradient1,
+                          ],
+                        ),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Contact",
+                                  //widget.contact.displayName,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Spaces.h4,
+                                Text(
+                                  //widget.contact.phones.toList()[0].value,
+                                  "1234567890",
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.info,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                            splashColor: Colors.red,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.call,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                            splashColor: Colors.red,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.message,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                            splashColor: Colors.black,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.close,
+                              color: Colors.white,
+                            ),
+                            onPressed: toggleMenuOptions,
+                            splashColor: Colors.red,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  Spaces.w12,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Contact",
-                          //widget.contact.displayName,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Spaces.h4,
-                        Text(
-                          //widget.contact.phones.toList()[0].value,
-                          "1234567890",
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
